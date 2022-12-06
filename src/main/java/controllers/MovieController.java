f1f7bf462c08059338f5fe14e0e051dedb72268f
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2022-12-06 20:50:09 +0000

    created getAllMovies GetMapping across MovieController and MovieService

--- src/main/java/controllers/MovieController.java
+++ src/main/java/controllers/MovieController.java
@@ -1,9 +1,15 @@
 package controllers;
 
+import models.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import services.MovieService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/movies")
@@ -11,6 +17,14 @@ public class MovieController {
 
     @Autowired
     MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        List<Movie> movies = movieService.getAllMovies();
+        return new ResponseEntity<>(movies, HttpStatus.OK);
+    }
+
+    
 
 
 
--- src/main/java/services/MovieService.java
+++ src/main/java/services/MovieService.java
@@ -1,8 +1,11 @@
 package services;
 
+import models.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repositories.MovieRepository;
+
+import java.util.List;
 
 @Service
 public class MovieService {
@@ -10,7 +13,13 @@ public class MovieService {
     @Autowired
     MovieRepository movieRepository;
 
-    
+    public MovieService(){}
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+
 
 }
 
